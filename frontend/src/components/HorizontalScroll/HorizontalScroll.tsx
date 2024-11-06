833b7f12b554c4c3cecfa08653167ff84dbcedc8
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2024-11-06 18:38:59 +0100

    #update

--- frontend/src/components/HorizontalScroll/HorizontalScroll.tsx
+++ frontend/src/components/HorizontalScroll/HorizontalScroll.tsx
@@ -1,61 +1,95 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useImperativeHandle,
+  forwardRef,
+} from "react";
 import style from "./HorizontalScroll.module.sass";
 
-const HorizontalScroll = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
-  const [scrollStartLeft, setScrollStartLeft] = useState(0);
-  const MAX_SCROLL_SPEED = 500;
-
-  const handleMouseDown = (e: React.MouseEvent) => {
-    if (!scrollRef.current) return;
-    setIsDragging(true);
-    setStartX(e.pageX);
-    setScrollStartLeft(scrollRef.current.scrollLeft);
-    e.preventDefault();
-  };
-
-  const handleMouseMove = (e: MouseEvent) => {
-    if (!isDragging || !scrollRef.current) return;
-    const x = e.pageX;
-    let walk = x - startX;
-    if (walk > MAX_SCROLL_SPEED) walk = MAX_SCROLL_SPEED;
-    else if (walk < -MAX_SCROLL_SPEED) walk = -MAX_SCROLL_SPEED;
-    scrollRef.current.scrollLeft = scrollStartLeft - walk;
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
-
-  useEffect(() => {
-    if (isDragging) {
-      window.addEventListener("mousemove", handleMouseMove);
-      window.addEventListener("mouseup", handleMouseUp);
-    } else {
-      window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("mouseup", handleMouseUp);
-    }
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("mouseup", handleMouseUp);
-    };
-  }, [isDragging]);
-
-  return (
-    <div
-      className={style.container}
-      ref={scrollRef}
-      onMouseDown={handleMouseDown}
-    >
-      {children}
-    </div>
-  );
+export type HorizontalScrollRef = {
+  scrollLeft: () => void;
+  scrollRight: () => void;
 };
+
+type HorizontalScrollProps = {
+  children: React.ReactNode;
+  scrollByAmount?: number;
+};
+
+const HorizontalScroll = forwardRef<HorizontalScrollRef, HorizontalScrollProps>(
+  ({ children, scrollByAmount }, ref) => {
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollStartLeft, setScrollStartLeft] = useState(0);
+    const MAX_SCROLL_SPEED = 500;
+
+    const handleMouseDown = (e: React.MouseEvent) => {
+      if (!scrollRef.current) return;
+      setIsDragging(true);
+      setStartX(e.pageX);
+      setScrollStartLeft(scrollRef.current.scrollLeft);
+      e.preventDefault();
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!isDragging || !scrollRef.current) return;
+      const x = e.pageX;
+      let walk = x - startX;
+      if (walk > MAX_SCROLL_SPEED) walk = MAX_SCROLL_SPEED;
+      else if (walk < -MAX_SCROLL_SPEED) walk = -MAX_SCROLL_SPEED;
+      scrollRef.current.scrollLeft = scrollStartLeft - walk;
+    };
+
+    const handleMouseUp = () => {
+      setIsDragging(false);
+    };
+
+    useEffect(() => {
+      if (isDragging) {
+        window.addEventListener("mousemove", handleMouseMove);
+        window.addEventListener("mouseup", handleMouseUp);
+      } else {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+      }
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+      };
+    }, [isDragging]);
+
+    const scrollByCardWidth = (direction: "left" | "right") => {
+      if (scrollRef.current) {
+        const scrollAmount = scrollByAmount ? scrollByAmount : 276;
+        const newScrollPosition =
+          direction === "left"
+            ? scrollRef.current.scrollLeft - scrollAmount
+            : scrollRef.current.scrollLeft + scrollAmount;
+
+        scrollRef.current.scrollTo({
+          left: newScrollPosition,
+          behavior: "smooth",
+        });
+      }
+    };
+
+    useImperativeHandle(ref, () => ({
+      scrollLeft: () => scrollByCardWidth("left"),
+      scrollRight: () => scrollByCardWidth("right"),
+    }));
+
+    return (
+      <div
+        className={style.container}
+        ref={scrollRef}
+        onMouseDown={handleMouseDown}
+      >
+        {children}
+      </div>
+    );
+  }
+);
 
 export default HorizontalScroll;
--- frontend/src/views/Home/Features/RecentlyAdded/RecentlyAdded.tsx
+++ frontend/src/views/Home/Features/RecentlyAdded/RecentlyAdded.tsx
@@ -1,13 +1,45 @@
+import { useRef } from "react";
 import style from "./RecentlyAdded.module.sass";
-import HorizontalScroll from "components/HorizontalScroll/HorizontalScroll";
+import HorizontalScroll, {
+  HorizontalScrollRef,
+} from "components/HorizontalScroll/HorizontalScroll";
 import MediaItem from "components/MediaItem/MediaItem";
+import { Icon } from "components/Icon/Icon";
 
 function RecentlyAdded({ items }: { items: any[] }) {
+  const horizontalScrollRef = useRef<HorizontalScrollRef>(null);
+
   return (
     <div className={style.container}>
-      <div className={style.title}>Recently added</div>
+      <div className={style.row}>
+        <div className={style.title}>Recently added</div>
+        <div className={style.buttons}>
+          <button
+            className={style.button}
+            onClick={() => horizontalScrollRef.current?.scrollLeft()}
+          >
+            <Icon
+              name={"basic_arrow"}
+              className={style.svg}
+              viewBox="4 4 24 24"
+              rotate="90deg"
+            />
+          </button>
+          <button
+            className={style.button}
+            onClick={() => horizontalScrollRef.current?.scrollRight()}
+          >
+            <Icon
+              name={"basic_arrow"}
+              className={style.svg}
+              viewBox="4 4 24 24"
+              rotate="270deg"
+            />
+          </button>
+        </div>
+      </div>
       <div className={style.separator} />
-      <HorizontalScroll>
+      <HorizontalScroll ref={horizontalScrollRef}>
         {items.map((item: any, index: number) => (
           <MediaItem
             key={index}
