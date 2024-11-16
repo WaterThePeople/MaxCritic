f5f57c0e382509eaac21da2bd1aaeba7797c9b59
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2024-11-16 19:07:21 +0100

    #user_image

--- backend/apps/main/admin.py
+++ backend/apps/main/admin.py
@@ -4,28 +4,73 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from django.contrib.auth.models import Group
 from .models import *
+from django.utils.safestring import mark_safe
+from django import forms
+from io import BytesIO
+from PIL import Image
 
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdminForm(forms.ModelForm):
+    image_file = forms.ImageField(required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password',
+                  'is_staff', 'is_superuser', 'image_file')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        image_file = self.cleaned_data.get('image_file')
+        if image_file:
+            img = Image.open(image_file)
+            img_byte_arr = BytesIO()
+            img.save(img_byte_arr, format='JPEG')
+            img_byte_arr.seek(0)
+
+            user.image = img_byte_arr.read()
+
+        if commit:
+            user.save()
+        return user
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    form = CustomUserAdminForm
+    readonly_fields = ('image_preview',)
     add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_filter = ()
-    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+    list_display = ('username',)
+
     fieldsets = (
-        (None, {"fields": ('username', "email", "password")}),
+        (None, {"fields": ('username', 'email', 'is_staff',
+         'is_superuser', 'image_file', 'image_preview')}),
     )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": (
-                'username', "email", "password1", "password2",
-            )}
-         ),
-    )
-    search_fields = ("username", "email",)
-    ordering = ("email",)
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+
+        if obj is None:
+            fieldsets = list(fieldsets)
+            fieldsets[0][1]['fields'] = fieldsets[0][1]['fields'] + \
+                ('password', )
+        else:
+            fieldsets = list(fieldsets)
+            fieldsets[0][1]['fields'] = tuple(
+                field for field in fieldsets[0][1]['fields'] if field != 'password'
+            )
+
+        return fieldsets
+
+    def image_preview(self, obj):
+        if obj.image:
+            image_base64 = base64.b64encode(obj.image).decode('utf-8')
+            return mark_safe(
+                f'<img src="data:image/jpeg;base64,{image_base64}" '
+                f'style="max-width: 200px; max-height: 200px;" />'
+            )
+        return "No image available."
+
+    image_preview.short_description = "Current Image"
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser, UserProfileAdmin)
 admin.site.unregister(Group)
--- backend/apps/main/models.py
+++ backend/apps/main/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
+import base64
 
 
 class CustomUser(AbstractUser):
@@ -12,6 +13,13 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ['email']
 
     objects = CustomUserManager()
+
+    image = models.BinaryField(blank=True, null=True)
+
+    def image_as_base64(self):
+        if self.image:
+            return base64.b64encode(self.image).decode('utf-8')
+        return None
 
     def __str__(self):
         return self.email
