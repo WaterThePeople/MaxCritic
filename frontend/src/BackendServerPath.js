cc289db484a4f5c9d33bbbf2765089b8a92c5891
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2024-12-09 15:33:54 +0100

    #updated_server_path

--- frontend/src/BackendServerPath.js
+++ frontend/src/BackendServerPath.js
@@ -1,2 +1,3 @@
 //export const serverPath = 'http://localhost:8000/';
-export const serverPath = 'https://maxcritic-production.up.railway.app/';
+//export const serverPath = 'https://maxcritic-production.up.railway.app/';
+export const serverPath = 'https://maxcritic-mrbx.onrender.com/';
