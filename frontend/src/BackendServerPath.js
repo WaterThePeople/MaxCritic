aeb1490c680090b3567ea7e905cf716b574f0fa5
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2025-01-29 15:20:43 +0100

    #changed_backend_url_to_local

--- frontend/src/BackendServerPath.js
+++ frontend/src/BackendServerPath.js
@@ -1,2 +1,2 @@
-//export const serverPath = 'http://localhost:8000/';
-export const serverPath = 'https://maxcritic-mrbx.onrender.com/';
+export const serverPath = 'http://localhost:8000/';
+//export const serverPath = 'https://maxcritic-mrbx.onrender.com/';
